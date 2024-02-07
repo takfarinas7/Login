c8232020a4ab7200fc218b3321cb5d9fb463f581
Takfarinas Djerroud <6225454@collegemv.qc.ca>
2024-02-07 16:18:36 -0500

    Ajout de fichiers

---
+++ pageCo.js
@@ -0,0 +1,7 @@
+function ajouter(){
+    let nom = document.getElementById("nom").value;
+    let mdp = document.getElementById("pswd").value;
+
+    localStorage.setItem('nom','mdp');
+    localStorage.setItem('nom','mdp');
+}
